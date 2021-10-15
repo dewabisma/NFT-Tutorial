e7ebe6c26bb7ade9f2865a0998b6eacce7d7e596
Dewa Bisma <idewagedebisma@gmail.com>
2021-10-15 15:04:16 +0800

    update env

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,14 @@
  */
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
-const { ALCHEMY_API, METAMASK_PRIVATE_KEY } = process.env;
+const { API_URL, METAMASK_PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.0",
   defaultNetwork: "ropsten",
   networks: {
     hardhat: {},
     ropsten: {
-      url: ALCHEMY_API,
+      url: API_URL,
       accounts: [`0x${METAMASK_PRIVATE_KEY}`],
     },
   },
